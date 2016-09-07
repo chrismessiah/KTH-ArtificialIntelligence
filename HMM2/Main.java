774e36af7d1ea0ed2d8a19e062b70c8c977a3481
Christian Abdelmassih <chrabd@kth.se>
2016-09-07 10:52:08 +0200

    HMM2 ready for kattis

--- HMM2/Main.java
+++ HMM2/Main.java
@@ -16,14 +16,14 @@ public class Main {
     // 1 4 1.0 0.0 0.0 0.0
     // 8 0 1 2 3 0 1 2 3
 
-    public static float[][] matrixMultiplier(float[][] A, float[][] B) {
+    public static double[][] matrixMultiplier(double[][] A, double[][] B) {
 
         if (A[0].length != B.length) {
             System.out.println("ERROR MATRIX DIMENSIONS INCORRECT! A: " + Integer.toString(A[0].length) + " B: " + Integer.toString(B.length) + "\nSHUTTING DOWN PROGRAM");
             System.exit(0);
         }
 
-        float[][] C = new float[A.length][B[0].length];
+        double[][] C = new double[A.length][B[0].length];
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B[0].length; j++) {
                 C[i][j] = 0;
@@ -40,7 +40,7 @@ public class Main {
         return C;
     }
 
-    public static void printArray(float[] array) {
+    public static void printArray(double[] array) {
         System.out.println(Arrays.toString(array));
     }
     
@@ -48,41 +48,38 @@ public class Main {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void printMatrix(float[][] matrix) {
+    public static void printMatrix(double[][] matrix) {
         System.out.println("");
         for (int i=0;i < matrix.length; i++) {
             printArray(matrix[i]);
         }
     }
 
-    public static float[][] vectorToMatrix(String[] vector, int rows, int columns) {
-        float[][] matrix = new float[rows][columns];
+    public static double[][] vectorToMatrix(String[] vector, int rows, int columns) {
+        double[][] matrix = new double[rows][columns];
         int k=0;
         for (int i=0; i<rows; i++) {
             for (int j=0; j<columns; j++) {
-                matrix[i][j] = Float.parseFloat(vector[k]);
+                matrix[i][j] = Double.parseDouble(vector[k]);
                 k+=1;
             }
         }
         return matrix;
     }
 
-    public static float[][] elementWiseProduct(float[][] vec1, float[][] vec2){
+    public static double[][] elementWiseProduct(double[][] vec1, double[][] vec2){
         
         /* OBS: include transpose function if vec1.length == 1. */
-
         if (vec1[0].length == 1){
             vec1 = transposeMatrix(vec1);
         }
 
         /* OBS: include transpose function if vec2.length == 1. */
-
         if (vec2[0].length == 1){
             vec2 = transposeMatrix(vec2);
         }
-
-
-        float[][] resultVector = new float[vec1[0].length][1];
+        
+        double[][] resultVector = new double[1][vec1[0].length];
 
         // checks that length is equal
         if (vec1[0].length != vec2[0].length){
@@ -91,23 +88,22 @@ public class Main {
         }
 
         // for each element, multiply
-        for (int i=0; i<vec1.length; i++) {
+        for (int i=0; i<vec1[0].length; i++) {
             resultVector[0][i] = vec1[0][i] * vec2[0][i];
         }
+        
         return resultVector;
-
-
     }
 
 
 
-    public static void printMatrixForKattis(float[][] matrix) {
+    public static void printMatrixForKattis(double[][] matrix) {
         int rows = matrix.length;
         int columns = matrix[0].length;
         String output = "" + Integer.toString(rows) + " " + Integer.toString(columns) + " ";
         for (int i=0; i<rows; i++) {
             for (int j=0;j<columns; j++) {
-                output += Float.toString(matrix[i][j]);
+                output += Double.toString(matrix[i][j]);
                 if (i+1 != rows || j+1 != columns) {
                    output += " ";
                 }
@@ -116,7 +112,7 @@ public class Main {
         System.out.println(output);
     }
 
-    public static float[][] getInputAsMatrix(BufferedReader input) throws IOException {
+    public static double[][] getInputAsMatrix(BufferedReader input) throws IOException {
 
         String inputData = input.readLine();
         String[] inputParts = inputData.split(" ");
@@ -124,24 +120,34 @@ public class Main {
         int rows = Integer.parseInt(inputParts[0]);
         int columns = Integer.parseInt(inputParts[1]);
         String[] inputVector = Arrays.copyOfRange(inputParts, 2, inputParts.length);
-        float[][] matrix = vectorToMatrix(inputVector, rows, columns);
+        double[][] matrix = vectorToMatrix(inputVector, rows, columns);
         return matrix;
     }
     
-    public static float[][] getColumnFromMatrix(float[][] matrix, int column) {
-        column -= 1;
-        float[][] columnVector = new float[matrix.length][1];
+    public static double[][] getInputAsMatrix(BufferedReader input, String inputString) throws IOException {
+
+        String[] inputParts = inputString.split(" ");
+        
+        int rows = Integer.parseInt(inputParts[0]);
+        int columns = Integer.parseInt(inputParts[1]);
+        String[] inputVector = Arrays.copyOfRange(inputParts, 2, inputParts.length);
+        double[][] matrix = vectorToMatrix(inputVector, rows, columns);
+        return matrix;
+    }
+    
+    public static double[][] getColumnFromMatrix(double[][] matrix, int column) {
+        double[][] columnVector = new double[matrix.length][1];
         for (int i=0;i<matrix.length; i++) {
             columnVector[i][0] = matrix[i][column];
         }
         return columnVector;
     }
     
-    public static float[][] transposeMatrix(float[][] matrix) {
+    public static double[][] transposeMatrix(double[][] matrix) {
         int rows = matrix.length;
         int columns = matrix[0].length;
 
-        float[][] newMatrix = new float[columns][rows];
+        double[][] newMatrix = new double[columns][rows];
 
         for(int i = 0; i < columns; i++) {
             for(int j = 0; j < rows; j++) {
@@ -164,42 +170,35 @@ public class Main {
         }
         return vector;
     }
+    
+    public static int[] getInputAsVector(BufferedReader input, String inputString) throws IOException {
+
+        String[] inputParts = inputString.split(" ");
+        
+        int numberOfElements = Integer.parseInt(inputParts[0]);
+        int[] vector = new int[numberOfElements];
+        for (int i=0; i<numberOfElements; i++) {
+            vector[i] = Integer.parseInt(inputParts[i+1]);
+        }
+        return vector;
+    }
 
     public static void main(String args[]) throws IOException {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-        //float[][] aMatrix = getInputAsMatrix(stdin);
-        float[][] bMatrix = getInputAsMatrix(stdin);
+        double[][] aMatrix = getInputAsMatrix(stdin, "4 4 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.1 0.1 0.1 0.0 0.8 0.8 0.1 0.1 0.0");
+        double[][] bMatrix = getInputAsMatrix(stdin, "4 4 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.0 0.0 0.0 0.9 0.1 0.1 0.0 0.0 0.9");
+        double[][] piMatrix = getInputAsMatrix(stdin, "1 4 1.0 0.0 0.0 0.0");
+        int[] obsSequence = getInputAsVector(stdin, "8 0 1 2 3 0 1 2 3");
 
         // get first column from inputMatrix
-        float[][] columnVector = getColumnFromMatrix(bMatrix, 1);
+        double[][] alpha = elementWiseProduct(piMatrix,getColumnFromMatrix(bMatrix, obsSequence[0]));
+        for (int i=1; i<obsSequence.length; i++) {
+            alpha = elementWiseProduct(matrixMultiplier(alpha, aMatrix), getColumnFromMatrix(bMatrix, obsSequence[i]));
+        }
         
-        float[][] piMatrix = getInputAsMatrix(stdin);
-        
-        //float[][] result = matrixMultiplier(piMatrix, bMatrix);
-        
-        printMatrix(bMatrix);
-        //printMatrix(result);
-        printMatrix(columnVector);
-        //int[] vector = getInputAsVector(stdin);
-        
+        System.out.println(alpha[0][obsSequence[obsSequence.length-1]]);
 
 
-
-        float[][] testElement = elementWiseProduct(piMatrix,columnVector);
-        printMatrix(testElement);
-
-
-
-
-        //printMatrix(aMatrix);
-        //printMatrix(bMatrix);
-        //printMatrix(columnVector);
-        //printMatrix(piMatrix);
-        //printArray(vector);
-
-        //float[][] resultVector = matrixMultiplier(piMatrix, aMatrix);
-        //float[][] resultVector2 = matrixMultiplier(resultVector, bMatrix);
-        //printMatrix(resultVector2);
     }
 
 }
